f91f1cf5b8d44bad599303ce9387b45e08a64de1
Vlad Kleshko <vkoesko@gmail.com>
2024-03-01 02:02:10 +0200

    Created 'create-profile/' url

--- user/urls.py
+++ user/urls.py
@@ -13,6 +13,7 @@ from user.views import (
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="create"),
+    path("create-profile/", UserProfileCreateView.as_view(), name="manage"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
