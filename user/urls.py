acdcb83c86a659a7a9c820411f600f04bcf4e58e
Vlad Kleshko <vkoesko@gmail.com>
2024-03-01 02:25:36 +0200

    Created 'profile/update/' url

--- user/urls.py
+++ user/urls.py
@@ -9,11 +9,13 @@ from rest_framework_simplejwt.views import (
 from user.views import (
     UserCreateView,
     UserProfileCreateView,
+    UserProfileUpdateView,
 )
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="create"),
-    path("create-profile/", UserProfileCreateView.as_view(), name="manage"),
+    path("profile/create/", UserProfileCreateView.as_view(), name="manage"),
+    path("profile/update/", UserProfileUpdateView.as_view(), name="manage"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
