3edec23c8d9b2c2d246a25a730cb01db5af74219
Vlad Kleshko <vkoesko@gmail.com>
2024-03-01 02:01:10 +0200

    Created UserProfileCreateView view

--- user/views.py
+++ user/views.py
@@ -1,7 +1,16 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
-from user.serializers import UserCreateSerializer
+from user.serializers import UserCreateSerializer, UserProfileSerializer
 
 
 class UserCreateView(CreateAPIView):
     serializer_class = UserCreateSerializer
+
+
+class UserProfileCreateView(CreateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        return self.request.user
