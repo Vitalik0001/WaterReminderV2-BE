293a7f4ab18592eecd5eeb56a5c0b87736066b28
Vlad Kleshko <vkoesko@gmail.com>
2024-03-03 14:16:12 +0200

    Created 'water/' url

--- water_reminder/urls.py
+++ water_reminder/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from water_reminder.views import DashboardView
+from water_reminder.views import DashboardView, WaterView
 
 urlpatterns = [
-    path("dashboard/", DashboardView.as_view(), name="dashboard")
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    path("water/", WaterView.as_view(), name="water")
+
 ]
 
 app_name = "water_reminder"
