278d469d08d90e7804c92836df0e01bfd271b3f0
Vlad Kleshko <vkoesko@gmail.com>
2024-03-07 00:11:40 +0200

    Refactor reset_daily_water_intake() to handle Water.DoesNotExist with try-except

--- water_reminder_app/tasks.py
+++ water_reminder_app/tasks.py
@@ -9,12 +9,15 @@ from water_reminder_app.models import Water, WaterLog
 def reset_daily_water_intake():
     users = get_user_model().objects.all()
     for user in users:
-        water, _ = Water.objects.get_or_create(user=user)
-        WaterLog.objects.filter(water=water).delete()
-        water.days += 1
-        water.daily_intake = 0
-        water.current_water_intake = 0
-        water.intake_status_percentage = 0
-        water.intake_achieved = False
-        water.last_updated = timezone.now()
-        water.save()
+        try:
+            water = Water.objects.get(user=user)
+            WaterLog.objects.filter(water=water).delete()
+            water.days += 1
+            water.daily_intake = 0
+            water.current_water_intake = 0
+            water.intake_status_percentage = 0
+            water.intake_achieved = False
+            water.last_updated = timezone.now()
+            water.save()
+        except Water.DoesNotExist:
+            pass
