e1abbc9f01e964dcf870647892119b9fb5f5e7f8
Vlad Kleshko <vkoesko@gmail.com>
2024-03-21 17:57:21 +0200

    Added GidrationTip to admin panel

--- water_reminder_app/admin.py
+++ water_reminder_app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from water_reminder_app.models import Water, WaterLog
+from water_reminder_app.models import (
+    Water,
+    WaterLog,
+    GidrationTip
+)
 
 admin.site.register(Water)
 admin.site.register(WaterLog)
+admin.site.register(GidrationTip)
